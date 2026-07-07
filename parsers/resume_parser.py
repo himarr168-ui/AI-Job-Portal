8193125f3f36cb6085a1e010d20419c3d07d0ee9
himarr168-ui <himarr168@gmail.com>
2026-07-07 15:47:53 +0530

    Day 5 Resume parser

---
+++ parsers/resume_parser.py
@@ -0,0 +1,34 @@
+import pdfplumber
+from docx import Document
+import os
+def read_pdf(resume_path):
+    text=""
+    with pdfplumber.open(resume_path) as pdf:
+        for page in pdf.pages:
+            page_text=page.extract_text()
+            if page_text:
+                text+=page.extract_text()+"\n"
+
+    return text
+def read_docx(file_path):
+    doc=Document(file_path)
+    text=""
+    for para in doc.paragraphs:
+        text+=para.text+"\n"
+    return text
+def clean_text(text):
+    text=text.replace("\n"," ")
+    text=" ".join(text.split())
+    return text
+
+def save_text(text, output_file):
+    with open(output_file,"w",encoding="utf-8") as file:
+        file.write(text)
+
+if __name__=="__main__":
+    resume_path="data\sampleresume.pdf"
+    text=read_pdf(resume_path)
+    print(text)
+    cleaned_text=clean_text(text)
+    save_text(cleaned_text,"data\clean_resume.txt")
+    print("Resume parsed successfully")
